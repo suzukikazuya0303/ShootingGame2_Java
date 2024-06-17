1685ecad2be0ddb781ac9114b7858d624293d061
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2024-06-17 10:56:59 +0900

    10-5のコミット

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -1,12 +1,25 @@
+import java.util.Vector;
 
 public class GameFrame extends MyFrame{
 	public void run() {
 		GameWorld.player = new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
+		GameWorld.playerBulles=new Vector<PlayerBullet>();
 		while(true) {
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
+			int i = 0;
+			while (i < GameWorld.playerBulles.size()) {
+				PlayerBullet b = GameWorld.playerBulles.get(i);
+				b.draw(this);
+				b.move();
+				if(b.y<0) {
+					GameWorld.playerBulles.remove(i);
+				}else {
+					i++;
+				}
+			}
 			sleep(0.03);
 		}
 		
