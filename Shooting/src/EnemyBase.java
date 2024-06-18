c0cc491ebdfbf3d0c1d30f050f645c33f295ccfb
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2024-06-18 10:47:27 +0900

    11-5のコミット

--- Shooting/src/EnemyBase.java
+++ Shooting/src/EnemyBase.java
@@ -10,12 +10,21 @@ public class EnemyBase extends Enemy{
 		if(Math.random()<0.05) {
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
 		}
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
+		}
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new DropEnemy(x,y,0,3));
+		}
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new CurveEnemy(x,y,0,4));
+		}
 		}
 		public void draw (MyFrame f) {
 			f.setColor(0, 128, 0);
-			f.fillRect(x, y,32,32);
+			f.fillOval(x, y,32,32);
 			f.setColor(200,200,200);
-			f.fillRect(x-16,y+8,64,16);
+			f.fillOval(x-16,y+8,64,16);
 		}
 	
 }
