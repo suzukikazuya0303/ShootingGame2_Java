6f933a9ee4cc9b46ce935c68d1b8f291bdc11da0
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2024-06-18 11:03:54 +0900

    練習問題11-1のこみっと

--- Shooting/src/EnemyBase.java
+++ Shooting/src/EnemyBase.java
@@ -8,11 +8,11 @@ public class EnemyBase extends Enemy{
 		if (x>300) vx=-1;
 		if (x<100) vx=1;
 		if(Math.random()<0.05) {
-			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
+			GameWorld.enemies.add(new StraightEnemy(x,y,100,2));
 		}
 		if(Math.random()<0.05) {
 			GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
-		}
+		}	
 		if(Math.random()<0.05) {
 			GameWorld.enemies.add(new DropEnemy(x,y,0,3));
 		}
