f49b1744b3dd1595682ba23a65fb92b4cdb5d1a7
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2024-06-18 10:33:20 +0900

    11-3のコミット

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -23,6 +23,10 @@ public class Player extends Character implements KeyListener{
 		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
 			GameWorld.playerBullets.add(
 					new PlayerBullet(x,y,0,-10));
+			GameWorld.playerBullets.add(
+					new PlayerBullet(x,y,2,-10));
+			GameWorld.playerBullets.add(
+					new PlayerBullet(x,y,-2,-10));
 			System.out.println("弾の数="+
 					GameWorld.playerBullets.size());
 		}
