edc34e5f588d72d07a9dabe7c808b40558dc728d
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2024-06-25 09:47:17 +0900

    練習問題12-1と、13章の13-1までの処理

--- Shooting/src/CurveEnemy.java
+++ Shooting/src/CurveEnemy.java
@@ -2,6 +2,8 @@
 public class CurveEnemy extends Enemy{
 	public CurveEnemy(double x,double y,double vx,double vy) {
 		super (x,y,vx,vy);
+		life = 3;
+		
 	}
 	public void move() {
 		super.move();
--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -16,6 +16,10 @@ public class GameFrame extends MyFrame{
 			moveEnemies();
 			checkPlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
+			if(GameWorld.enemies.size()==0) {
+				setColor(0,0,0);
+				drawString("クリア！",100,200,40);
+			}
 			sleep(0.03);
 			
 		}
@@ -69,6 +73,15 @@ public class GameFrame extends MyFrame{
 			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+		int i = 0;
+		while(i<GameWorld.enemies.size()) {
+			Enemy e = GameWorld.enemies.get(i);
+			if(e.y>400) {
+				GameWorld.enemies.remove(i);
+			}else {
+				i++;
+			}
 		}
 		
 	}
