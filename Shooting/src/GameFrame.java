50ccd76e6ef4f1fbb40f76298d34672ac00078fd
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2024-06-25 10:07:27 +0900

    13-5,6のコミット

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -5,6 +5,7 @@ public class GameFrame extends MyFrame{
 		GameWorld.player = new Player(100,300,0,0);
 		GameWorld.player.draw(this);
 		addKeyListener(GameWorld.player);
+		GameWorld.stage = 1;
 		while(true) {
 			GameWorld.player.x = 100;
 			GameWorld.player.y = 300;
@@ -13,6 +14,7 @@ public class GameFrame extends MyFrame{
 		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
 		while(true) {
 			clear();
+			drawString("Stage = "+GameWorld.stage,300,50,15);
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
@@ -23,12 +25,14 @@ public class GameFrame extends MyFrame{
 				setColor(0,0,0);
 				drawString("クリア！",100,200,40);
 				if(GameWorld.enterPressed) {
+					GameWorld.stage++;
 					break;
 				}
 			}else if(GameWorld.player.y<0){
 				setColor(0,0,0);
 				drawString("ゲームオーバー！",50,200,40);
 				if(GameWorld.enterPressed) {
+					GameWorld.stage = 1;
 					break;
 				}
 			}
